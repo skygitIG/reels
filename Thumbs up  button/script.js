9e48f9f2bd36d8f172bcc6709ae24cc2ea803046
Akash Kumar <117715724+skygitIG@users.noreply.github.com>
2023-05-16 14:13:08 +0530

    Create script.js

---
+++ Thumbs up  button/script.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.button').forEach(button => {
+
+    button.addEventListener('click', e => {
+        button.classList.toggle('liked');
+        if(button.classList.contains('liked')) {
+            gsap.fromTo(button, {
+                '--hand-rotate': 8
+            }, {
+                ease: 'none',
+                keyframes: [{
+                    '--hand-rotate': -45,
+                    duration: .16,
+                    ease: 'none'
+                }, {
+                    '--hand-rotate': 15,
+                    duration: .12,
+                    ease: 'none'
+                }, {
+                    '--hand-rotate': 0,
+                    duration: .2,
+                    ease: 'none',
+                    clearProps: true
+                }]
+            });
+        }
+    })
+
+});
